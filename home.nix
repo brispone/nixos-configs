acbf51afa686376c86a2ea97e7a8e351cb2b845a
Brad Rispone <bradrispone@gmail.com>
2024-03-01 10:26:47 -0600

    Add VLC to home-manager

--- home.nix
+++ home.nix
@@ -22,6 +22,7 @@
     neofetch
     git
     cava
+    vlc
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
